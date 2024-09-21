d1b1226056fc2e5fb5819c6c1d8cece3ca2ef02d
krish <krishmaniyar27@gmail.com>
2024-09-21 17:13:47 +0530

    index.html

---
+++ javascript.js
@@ -0,0 +1,79 @@
+let gridNum = 2;
+const container = document.getElementById("container");
+const add_book_form = document.getElementById("erase");
+
+let myLibrary = [];
+
+const newBook1 = new Book("Atomic Habit", "James Clear", 1, 285);
+const newBook2 = new Book("Zero To One", "Peter Thiel", 2, 186);
+myLibrary.push(newBook1);
+myLibrary.push(newBook2);
+
+makeGrid();
+
+function Book(name, author, serial_no, pages) {
+    this.name = name;
+    this.author = author;
+    this.serial_no = serial_no;
+    this.pages = pages;
+}
+
+function addBookToLibrary() {
+    const name = document.getElementById("book_name").value;
+    const author = document.getElementById("author_name").value;
+    const serial_no = document.getElementById("serial_no").value;
+    const pages = document.getElementById("no_of_pages").value;
+
+    const newBook = new Book(name, author, serial_no, pages);
+    myLibrary.push(newBook);
+    console.log(myLibrary[gridNum].name);
+    gridNum++;
+    console.log(gridNum);
+    makeGrid();
+}
+
+
+function add_book()
+{
+    document.getElementById("container").innerHTML="";
+    container.append(add_book_form);
+}
+
+function makeGrid()
+{
+    add_book_form.remove();
+    for (i=0;i<gridNum;i++)
+    {
+        let newElement = document.createElement("div");
+        container.appendChild(newElement).className = "grid-item";
+        if(gridNum > 3)
+        {
+            newElement.style.maxWidth = "28%";
+        }
+        else
+        {
+            newElement.style.maxWidth = "" + 100/gridNum + "%";
+        }
+        newElement.innerHTML = "<h3>Name:" + myLibrary[i].name + "<br>Author Name:" + myLibrary[i].author + "<br>Serial No: " + myLibrary[i].serial_no + "<br>No of Pages:  " + myLibrary[i].pages + "</h3>";
+        newElement.innerHTML += "<button type='button' onclick='remove(" + i + ")' style='border-radius: 10px; background-color: beige;font-size: 1.3em;padding: 1%; margin-top:10px;'>Delete</button>"
+    }
+}
+
+document.getElementById("add").addEventListener("click",()=>
+{
+    add_book();
+    document.getElementById("detail").reset();
+});
+
+function library()
+{
+    addBookToLibrary();
+}
+
+function remove(j)
+{
+    myLibrary.splice(j,j);
+    document.getElementById("container").innerHTML="";
+    gridNum--;
+    makeGrid();
+}
