39f6912304a5e55b05ccc42c74c9f3f3160b09f3
krish <krishmaniyar27@gmail.com>
2024-12-16 21:55:29 +0530

    index.html

--- javascript.js
+++ javascript.js
@@ -2,12 +2,22 @@ let gridNum = 3;
 const container = document.getElementById("container");
 const add_book_form = document.getElementById("erase");
 
-let myLibrary = [];
+var myLibrary = [];
+
+class Book {
+    constructor (name, author, serial_no, pages,status) {
+        this.name = name;
+        this.author = author;
+        this.serial_no = serial_no;
+        this.pages = pages;
+        this.status = status;
+    }    
+}
 
 function insertbook() {
-    const newBook0 = new Book("","",0,0);
-    const newBook1 = new Book("Atomic Habit", "James Clear", 1, 285);
-    const newBook2 = new Book("Zero To One", "Peter Thiel", 2, 186);
+    const newBook0 = new Book("Atomic Habit", "James Clear", 1, 285,true);
+    const newBook1 = new Book("How-to-Win-Friends-and-Influence-People", "Dale Carnegie", 2, 263,false);
+    const newBook2 = new Book("Zero To One", "Peter Thiel", 3, 186,true);
     myLibrary.push(newBook0);
     myLibrary.push(newBook1);
     myLibrary.push(newBook2);
@@ -17,20 +27,22 @@ insertbook();
 
 makeGrid();
 
-function Book(name, author, serial_no, pages,status) {
-    this.name = name;
-    this.author = author;
-    this.serial_no = serial_no;
-    this.pages = pages;
-}
+
+// function Book(name, author, serial_no, pages,status) {
+//     this.name = name;
+//     this.author = author;
+//     this.serial_no = serial_no;
+//     this.pages = pages;
+// }
 
 function addBookToLibrary() {
     const name = document.getElementById("book_name").value;
     const author = document.getElementById("author_name").value;
     const serial_no = document.getElementById("serial_no").value;
     const pages = document.getElementById("no_of_pages").value;
+    const status = document.getElementById("checkbox");
 
-    const newBook = new Book(name, author, serial_no, pages);
+    const newBook = new Book(name, author, serial_no, pages,status.checked);
     myLibrary.push(newBook);
     console.log(myLibrary[gridNum].name);
     gridNum++;
@@ -46,13 +58,64 @@ function add_book()
     container.append(add_book_form);
 }
 
+// Grid for read books only
+
+function makeGridIfRead()
+{
+    add_book_form.remove();
+    document.getElementById("container").innerHTML="";
+    for (i=0;i<gridNum;i++)
+    {
+        if(myLibrary[i].status)
+        {
+            let newElement = document.createElement("div");
+            container.appendChild(newElement).className = "grid-item";
+            if(gridNum > 3)
+            {
+                newElement.style.maxWidth = "28%";
+            }
+            else
+            {
+                newElement.style.maxWidth = "" + 100/gridNum + "%";
+            }
+            newElement.innerHTML = "<h3>Name: " + myLibrary[i].name + "<br>Author Name: " + myLibrary[i].author + "<br>Serial No: " + myLibrary[i].serial_no + "<br>No of Pages:  " + myLibrary[i].pages + "<br>Read: " + myLibrary[i].status + "</h3>";
+        }
+    }
+}
+
+// Grid for Unread books
+
+function makeGridIfUnread()
+{
+    add_book_form.remove();
+    document.getElementById("container").innerHTML="";
+    for (i=0;i<gridNum;i++)
+    {
+        if(!myLibrary[i].status)
+        {
+            let newElement = document.createElement("div");
+            container.appendChild(newElement).className = "grid-item";
+            if(gridNum > 3)
+            {
+                newElement.style.maxWidth = "28%";
+            }
+            else
+            {
+                newElement.style.maxWidth = "" + 100/gridNum + "%";
+            }
+            newElement.innerHTML = "<h3>Name: " + myLibrary[i].name + "<br>Author Name: " + myLibrary[i].author + "<br>Serial No: " + myLibrary[i].serial_no + "<br>No of Pages:  " + myLibrary[i].pages + "<br>Read: " + myLibrary[i].status + "</h3>";
+        }
+    }
+}
+
 // Default page
+
 
 function makeGrid()
 {
     add_book_form.remove();
     document.getElementById("container").innerHTML="";
-    for (i=1;i<gridNum;i++)
+    for (i=0;i<gridNum;i++)
     {
         let newElement = document.createElement("div");
         container.appendChild(newElement).className = "grid-item";
@@ -64,7 +127,7 @@ function makeGrid()
         {
             newElement.style.maxWidth = "" + 100/gridNum + "%";
         }
-        newElement.innerHTML = "<h3>Name: " + myLibrary[i].name + "<br>Author Name: " + myLibrary[i].author + "<br>Serial No: " + myLibrary[i].serial_no + "<br>No of Pages:  " + myLibrary[i].pages + "</h3>";
+        newElement.innerHTML = "<h3>Name: " + myLibrary[i].name + "<br>Author Name: " + myLibrary[i].author + "<br>Serial No: " + myLibrary[i].serial_no + "<br>No of Pages:  " + myLibrary[i].pages + "<br>Read: " + myLibrary[i].status + "</h3>";
     }
 }
 
@@ -72,7 +135,7 @@ function makeGridDelete()
 {
     add_book_form.remove();
     document.getElementById("container").innerHTML="";
-    for (i=1;i<gridNum;i++)
+    for (i=0;i<gridNum;i++)
     {
         let newElement = document.createElement("div");
         container.appendChild(newElement).className = "grid-item";
@@ -113,6 +176,9 @@ function library()
 function remove(j)
 {
     console.log(j);
+    if(j==0)
+    console.log(myLibrary.splice(0,1));
+    else
     console.log(myLibrary.splice(j,j));
     document.getElementById("container").innerHTML="";
     gridNum--;
